5fd8eba4c09a77aa9d3cac4fd8fbaa72e3da0972
funnyfeet434 <65707340+funnyfeet434@users.noreply.github.com>
2020-05-28 13:59:39 +0200

    added testpy

---
+++ Test.py
@@ -0,0 +1,4 @@
+import numpy as np 
+
+mylist = np.array([range(i, i + 3) for i in [2, 4, 6]])
+print(mylist)
--- linear_arithmatic/vectors.py
+++ linear_arithmatic/vectors.py
@@ -9,5 +9,5 @@ distance_travelled_b2 = np.sqrt(v_b2[0]**2 + v_b2[1])
 dif_b1nb2 = abs(v_b1 - v_b2)
 distance_b1nb2 = np.sqrt(dif_b1nb2[0]**2 + dif_b1nb2[1]**2)
 
-print('The first ball travelled {0} m more.'.format(round(distance_travelled_b1-distance_travelled_b2), 1))
+print('The first ball travelled {0} m more.'.format(round(distance_travelled_b1-distance_travelled_b2, 1)))
 print('The distance between the two balls is ' + str(round(distance_b1nb2, 1)))
